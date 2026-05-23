2f7f02e266e50db7e338d81cb74985c59cd0d399
Ricardo Miranda <miranda9.ricardo@gmail.com>
2026-05-23 16:21:26 -0700

    Initial Commit

---
+++ Solar Simulation/Solar_SimulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Solar_SimulationApp.swift
+//  Solar Simulation
+//
+//  Created by Ricardo Miranda on 5/23/26.
+//
+
+import SwiftUI
+
+@main
+struct Solar_SimulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
